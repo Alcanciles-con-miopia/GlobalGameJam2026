d079d714eb80afe01dafffb58280043855a7b22e
Cynthia Tristán <cyntrist@ucm.es>
2026-01-31 21:28:41 +0100

    XD

--- scripts/draggableObject.gd
+++ scripts/draggableObject.gd
@@ -2,23 +2,40 @@ extends Node
 
 var cam: Camera3D = null
 var dif
+const RAY_LENGTH = 100000.0
 
 var mode: int = 1
 var scaleChange: float = 1.0
 
 # Start.
 func _ready():
-	cam = get_tree().get_first_node_in_group("game_camera") as Camera3D
+	#cam = get_tree().get_first_node_in_group("game_camera") as Camera3D
+	cam = $"../../../..".getCamRef()
 	if cam == null:
 		push_error("DraggableObject: no se encontró ninguna Camera3D en el grupo 'game_camera'")
 	
-	if $"../" is Node3D:
-		dif = $"../".global_position - self.global_position
+	dif = $"../".global_position - self.global_position
+	pass
+
+# Update.
+func _process(_delta):
+	pass
+	
+
+func raycast():
+	var spaceState = cam.get_world_3d().direct_space_state
+	var mousePos = cam.get_viewport().get_mouse_position()
+	var origin = cam.project_ray_origin(mousePos)
+	var end = origin + cam.project_ray_normal(mousePos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	var result = spaceState.intersect_ray(query)
+	result = result.get("collider")
+	#print(result)
+	return result
 
 #Input.
-func _input(event) -> void:
-	if cam == null:
-		return
+func _input(event):
 	
 	if event is InputEventKey and event.pressed :
 		match event.keycode:
@@ -33,39 +50,35 @@ func _input(event) -> void:
 				print("MODO: ESCALAR")
 		
 	if Input.is_action_pressed("left_click"):
-		var collider = cam.raycast()
+		var collider = raycast()
 		if collider != null:
 			Global.draggingSomething = collider
-			# print("Arrastrando:", Global.draggingSomething)
+			print("Arrastrando:", Global.draggingSomething)
 			
-	if Global.draggingSomething == self and Global.draggingSomething != null:
-		if event is InputEventKey and event.pressed:
-			var parent_node := $"../" as Node3D
-			if parent_node == null:
-				return
-				
-			match event.keycode:
-				KEY_A:
-					if mode == 1:
-						parent_node.global_position += Vector3(-0.1, 0.0, 0.0)
-					elif mode == 2:
-						parent_node.transform = parent_node.transform.rotated(Vector3(0.0, 1.0, 0.0), -0.1)
-				KEY_D:
-					if mode == 1:
-						parent_node.global_position += Vector3(0.1, 0.0, 0.0)
-					elif mode == 2:
-						parent_node.transform = parent_node.transform.rotated(Vector3(0.0, 1.0, 0.0), 0.1)
-				KEY_W:
-					if mode == 1:
-						parent_node.global_position += Vector3(0.0, 0.1, 0.0)
-					elif mode == 3:
-						parent_node.transform = parent_node.transform.scaled(Vector3(1.1, 1.1, 1.1))
-				KEY_S:
-					if mode == 1:
-						parent_node.global_position += Vector3(0.0, -0.1, 0.0)
-					elif mode == 3:
-						parent_node.transform = parent_node.transform.scaled(Vector3(0.9, 0.9, 0.9))
-
+	if (Global.draggingSomething == self and Global.draggingSomething != null):
+		if event is InputEventKey and event.pressed :
+			if event.keycode == KEY_A:
+				if mode == 1:
+					$"../".global_position += Vector3(-0.1, 0.0, 0.0)
+				elif mode == 2:
+					$"../".transform = $"../".transform.rotated(Vector3(0.0, 1.0, 0.0), -0.1)
+			elif event.keycode == KEY_D:
+				if mode == 1:
+					$"../".global_position += Vector3(0.1, 0.0, 0.0)
+				elif mode == 2:
+					$"../".transform = $"../".transform.rotated(Vector3(0.0, 1.0, 0.0), 0.1)
+			elif event.keycode == KEY_W:
+				if mode == 1:
+					$"../".global_position += Vector3(0.0, 0.1, 0.0)
+				elif mode == 3:
+					$"../".transform = $"../".transform.scaled(Vector3(1.1, 1.1, 1.1))
+			elif event.keycode == KEY_S:
+				if mode == 1:
+					$"../".global_position += Vector3(0.0, -0.1, 0.0)
+				elif mode == 3:
+					$"../".transform = $"../".transform.scaled(Vector3(0.9, 0.9, 0.9))
+			pass
+	
 	# Quitar el click.
 	if Input.is_action_pressed("right_click") and Global.draggingSomething != self:
 		Global.draggingSomething = null
@@ -83,3 +96,4 @@ func _input(event) -> void:
 	# Quitar el click.
 	#if Input.is_action_just_released("left_click") and Global.draggingSomething == self:
 		#Global.draggingSomething = null
+	pass
--- scripts/raycaster.gd
+++ scripts/raycaster.gd
@@ -1,16 +1,10 @@
-extends Camera3D
+extends Node3D
+var cam
 
-const RAY_LENGTH := 100000.0
+func _ready():
+	cam = self
+	pass
 
-func getCamRef() -> Camera3D:
-	return self
-
-func raycast():
-	var space_state = get_world_3d().direct_space_state
-	var mouse_pos = get_viewport().get_mouse_position()
-	var origin = project_ray_origin(mouse_pos)
-	var end = origin + project_ray_normal(mouse_pos) * RAY_LENGTH
-	var query := PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	var result = space_state.intersect_ray(query)
-	return result.get("collider")
+func _process(delta):
+	#raycast()
+	pass
