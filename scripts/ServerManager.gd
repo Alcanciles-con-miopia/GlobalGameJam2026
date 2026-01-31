d8fdaf8115d90436381ce3f35637d49f67758da6
Cynthia Tristán <cyntrist@ucm.es>
2026-01-31 11:20:00 +0100

    debugs

--- scripts/ServerManager.gd
+++ scripts/ServerManager.gd
@@ -29,6 +29,7 @@ func CreateClient(_address: String = addres, _port: String = port):
 	print("CONECTADO...")
 	
 func player_connected(id: int = 1):
+	print_debug("Player ", id, " conectado.")
 	pass
 	#var player = Player.instantiate()
 	#player.name = str(id)
