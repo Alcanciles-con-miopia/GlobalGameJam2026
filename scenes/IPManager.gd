0d541cd50d50891f7b8b549d899cc7000f68b46a
Cynthia Tristán <cyntrist@ucm.es>
2026-01-31 13:29:58 +0100

    Update IPManager.gd

--- scenes/IPManager.gd
+++ scenes/IPManager.gd
@@ -4,8 +4,7 @@ var HostIP
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var ip = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
-	text = ip
+	pass
 	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
