b49714618c038138e12784b689562716d8695ea1
Cynthia Tristán <cyntrist@ucm.es>
2026-01-31 13:31:50 +0100

    Revert "Update IPManager.gd"
    This reverts commit 0d541cd50d50891f7b8b549d899cc7000f68b46a.

--- scenes/IPManager.gd
+++ scenes/IPManager.gd
@@ -4,7 +4,8 @@ var HostIP
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	var ip = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
+	text = ip
 	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
