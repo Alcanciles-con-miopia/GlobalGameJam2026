e446962858af780f2b731f0a421ab74359fb29fc
Cynthia Tristán <cyntrist@ucm.es>
2026-02-01 13:51:02 +0100

    Update Countdown.gd

--- scripts/Countdown.gd
+++ scripts/Countdown.gd
@@ -2,14 +2,14 @@ extends Timer
 
 @onready var TimerLabel: Label = $Label
 
-
 func _process(delta: float) -> void:
 	var time = get_time_left()
 
 	if(time > 0):
 		var seconds:float = fmod(time , 60.0)
 		var minutes:int   =  int(time / 60.0) % 60
-		var hhmmss_string:String = "%02d:%05.2f" % [minutes, seconds]
+		#TimerLabel.text = "%02d.%02d" % [seconds, centesimas]
+		var hhmmss_string:String = "%05.2f" % [seconds]
 		TimerLabel.text = hhmmss_string
 	else:
 		TimerLabel.text = "00:00"
